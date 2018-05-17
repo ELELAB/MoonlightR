c96c927096fe3713dce88246bde53497e5726090
colsen <colsen@ulb.ac.be>
2018-05-17 16:45:47 +0200

    resolve DOSE error

--- R/getDataTCGA.R
+++ R/getDataTCGA.R
@@ -1,10 +1,9 @@
 #' getDataTCGA
 #'
-#' This function retrieves and prepares TCGA data (hg19)
+#' This function retrieves and prepares TCGA data
 #' @param cancerType select cancer type for which analysis should be run. panCancer
 #' 		for all available cancer types in TCGA. Defaults to panCancer
-#' @param dataType is dataType such as gene expression, cnv, DNA methylation etc. 
-#' Options: "Methylation" (27k) and "Gene expression"
+#' @param dataType is dataType such as gene expression, cnv, methylation etc.
 #' @param directory	Directory/Folder where the data was downloaded. Default: GDCdata
 #' @param cor.cut cor.cut 
 #' @param qnt.cut qnt.cut 
@@ -28,149 +27,152 @@
 #' @examples
 #' \dontrun{
 #' dataFilt <- getDataTCGA(cancerType = "LUAD", 
-#'                         dataType = "Gene expression", 
-#'                         directory = "data", 
-#'                         nSample = 4)
+#' dataType = "Gene expression", directory = "data", nSample = 4)
 #' }
+
 getDataTCGA <- function(cancerType, dataType, directory, 
-                        cor.cut = 0.6, qnt.cut = 0.25, 
-                        nSample, stage = "ALL", subtype = 0, 
-                        samples = NULL, seed = 12345){
-  
+                          cor.cut = 0.6, qnt.cut = 0.25, 
+                          nSample, stage = "ALL", subtype = 0, 
+                          samples = NULL, seed = 12345){
+
   DiseaseList <- get("DiseaseList")
   GDCprojects <- get("GDCprojects")
   geneInfo <- get("geneInfo")
   
-  set.seed(seed)
-  CancerProject <- paste0("TCGA-",cancerType)
-  DataDirectory <- paste0(directory,"GDC_",gsub("-","_",CancerProject))
-  
-  if(dataType == "Gene expression"){
-    
-    FileNameData <- paste0(DataDirectory, "_stage_", stage,
-                           "_subtype_", subtype,
-                           "_","IlluminaHiSeq",".rda")
-    if(!file.exists(FileNameData)){
-      query <- GDCquery(project = CancerProject, 
-                        data.category = "Gene expression",
-                        data.type = "Gene expression quantification",
-                        platform = "Illumina HiSeq", 
-                        file.type = "results",
-                        legacy = TRUE)
-      
-      samplesDown <- query$results[[1]]$cases
-      
-      dataSmTP <- TCGAquery_SampleTypes(barcode = samplesDown,
-                                        typesample = "TP")
-      
-      dataSmNT <- TCGAquery_SampleTypes(barcode = samplesDown,
-                                        typesample = "NT")
-      
-      if (is.numeric(stage)==TRUE){
-        dataClin <- GDCquery_clinic(project = CancerProject, type = "clinical")
-        curStage <- paste0("Stage ", as.roman(stage))
-        dataClin$tumor_stage <- toupper(dataClin$tumor_stage)
-        dataClin$tumor_stage <- gsub("[ABCDEFGH]","",dataClin$tumor_stage)
-        dataClin$tumor_stage <- gsub("ST","Stage",dataClin$tumor_stage)
-        
-        dataStg <- dataClin[dataClin$tumor_stage %in% curStage,]
-        message(paste(curStage, "with", nrow(dataStg), "samples"))
-        
-        dataStgC <- dataSmTP[substr(dataSmTP,1,12) %in% dataStg$bcr_patient_barcode]
-        dataSmTP <- dataStgC
-      }
-      
-      if (is.character(subtype)==TRUE){
-        dataSmSubt <- dataSmTP[substr(dataSmTP,1,12) %in% samples]
-        dataSmTP <- dataSmSubt
-      }
-      
-      message(paste0(CancerProject," ... with ", length(dataSmTP), " Primary solid Tumor, ", length(dataSmNT), " Solid Tissue Normal"))
-      
-      if(is.null(nSample)){
-        queryDown <- GDCquery(project = CancerProject, 
-                              data.category = "Gene expression",
-                              data.type = "Gene expression quantification",
-                              platform = "Illumina HiSeq", 
-                              file.type = "results",
-                              barcode = c(dataSmTP, dataSmNT),
-                              # barcode = c(sample(dataSmTP,nSample), sample(dataSmNT,nSample)),
-                              legacy = TRUE)
-      } else {
-        queryDown <- GDCquery(project = CancerProject, 
-                              data.category = "Gene expression",
-                              data.type = "Gene expression quantification",
-                              platform = "Illumina HiSeq", 
-                              file.type = "results",
-                              # barcode = c(dataSmTP, dataSmNT),
-                              barcode = c(sample(dataSmTP,nSample), sample(dataSmNT,nSample)),
-                              legacy = TRUE)
-      }
-      
-      GDCdownload(queryDown, directory = DataDirectory)
-      
-      RSEobject <- GDCprepare(query = queryDown, 
-                              save = TRUE, 
-                              directory =  DataDirectory,
-                              save.filename = FileNameData)
-    } else {
-      RSEobject <- get(load(FileNameData))
-    }
-    
-    dataPrep <- TCGAanalyze_Preprocessing(object = RSEobject,
-                                          cor.cut = cor.cut,
-                                          datatype = "raw_count")                      
-    
-    dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
-                                          geneInfo = geneInfo,
-                                          method = "gcContent")                
-    
-    dataFilt <- TCGAanalyze_Filtering(tabDF = dataNorm,
-                                      method = "quantile", 
-                                      qnt.cut =  qnt.cut)  
-    
-    return(dataFilt)
-    
-  } else if (dataType == "Methylation"){
-    
-    FileNameData <- paste0(DataDirectory, "_","Illumina Human Methylation 27",".rda")
-    
-    if(!file.exists(FileNameData)){
-      query.met <- GDCquery(project = CancerProject, 
-                            legacy = TRUE,
-                            data.category = "DNA methylation",
-                            platform = "Illumina Human Methylation 27")
-      
-      query.met.samples <-  query.met$results[[1]]$cases
-      
-      dataSmTP <- TCGAquery_SampleTypes(query.met.samples,"TP")
-      dataSmNT <- TCGAquery_SampleTypes(query.met.samples,"NT")
-      
-      message(CancerProject," ... with ", length(dataSmTP), " Primary solid Tumor, ", length(dataSmNT), " Solid Tissue Normal")
+    set.seed(seed)
+    CancerProject <- paste0("TCGA-",cancerType)
+    DataDirectory <- paste0(directory,"GDC_",gsub("-","_",CancerProject))
 
-      if(is.null(nSample)){
-        sampDown <- c(dataSmTP, dataSmNT)
-      } else {
-        sampDown <- c(sample(dataSmTP,nSample), sample(dataSmNT,nSample))
-      }
+    if(dataType == "Gene expression"){
+
+        FileNameData <- paste0(DataDirectory, "_stage_", stage,
+                               "_subtype_", subtype,
+                               "_","IlluminaHiSeq",".rda")
+        if(!file.exists(FileNameData)){
+            query <- GDCquery(project = CancerProject, 
+                              data.category = "Gene expression",
+                              data.type = "Gene expression quantification",
+                              platform = "Illumina HiSeq", 
+                              file.type = "results",
+                              legacy = TRUE)
+
+            samplesDown <- query$results[[1]]$cases
+
+            dataSmTP <- TCGAquery_SampleTypes(barcode = samplesDown,
+                                              typesample = "TP")
+
+            dataSmNT <- TCGAquery_SampleTypes(barcode = samplesDown,
+                                              typesample = "NT")
+            
+            if (is.numeric(stage)==TRUE){
+                dataClin <- GDCquery_clinic(project = CancerProject,type = "clinical_patient")
+                curStage <- paste0("Stage ", as.roman(stage))
+                dataClin$tumor_stage <- toupper(dataClin$tumor_stage)
+                dataClin$tumor_stage <- gsub("[ABCDEFGH]","",dataClin$tumor_stage)
+                dataClin$tumor_stage <- gsub("ST","Stage",dataClin$tumor_stage)
+
+                dataStg <- dataClin[dataClin$tumor_stage %in% curStage,]
+                message(paste(curStage, "with", nrow(dataStg), "samples"))
+
+                dataStgC <- dataSmTP[substr(dataSmTP,1,12) %in% dataStg$bcr_patient_barcode]
+                dataSmTP <- dataStgC
+            }
+            
+            if (is.character(subtype)==TRUE){
+              dataSmSubt <- dataSmTP[substr(dataSmTP,1,12) %in% samples]
+              dataSmTP <- dataSmSubt
+            }
+            
+            message <- paste0(CancerProject," ... with ", length(dataSmTP), " TP, ", length(dataSmNT), " NT")
+            print(message)
+
+            if(is.null(nSample)){
+                queryDown <- GDCquery(project = CancerProject, 
+                                  data.category = "Gene expression",
+                                  data.type = "Gene expression quantification",
+                                  platform = "Illumina HiSeq", 
+                                  file.type = "results",
+                                  barcode = c(dataSmTP, dataSmNT),
+                                  # barcode = c(sample(dataSmTP,nSample), sample(dataSmNT,nSample)),
+                                  legacy = TRUE)
+            }else{
+                queryDown <- GDCquery(project = CancerProject, 
+                                  data.category = "Gene expression",
+                                  data.type = "Gene expression quantification",
+                                  platform = "Illumina HiSeq", 
+                                  file.type = "results",
+                                  # barcode = c(dataSmTP, dataSmNT),
+                                  barcode = c(sample(dataSmTP,nSample), sample(dataSmNT,nSample)),
+                                  legacy = TRUE)
+            }
+
+            GDCdownload(queryDown, directory = DataDirectory)
+
+            RSEobject <- GDCprepare(query = queryDown, 
+                                   save = TRUE, 
+                                   directory =  DataDirectory,
+                                   save.filename = FileNameData)
+        }else{
+            load(FileNameData)
+            data <- get("data")
+            RSEobject <- data
+        }
+
+        dataPrep <- TCGAanalyze_Preprocessing(object = RSEobject,
+                                              cor.cut = cor.cut,
+                                              datatype = "raw_count")                      
+
+        dataNorm <- TCGAanalyze_Normalization(tabDF = dataPrep,
+                                              geneInfo = geneInfo,
+                                              method = "gcContent")                
+
+        dataFilt <- TCGAanalyze_Filtering(tabDF = dataNorm,
+                                          method = "quantile", 
+                                          qnt.cut =  qnt.cut)  
+
+        return(dataFilt)
+
+    }else if(dataType == "Methylation"){
+        FileNameData <- paste0(DataDirectory, "_","Illumina Human Methylation 27",".rda")
+        if(!file.exists(FileNameData)){
+            query.met <- GDCquery(project = CancerProject, 
+                                legacy = TRUE,
+                                data.category = "DNA methylation",
+                                platform = "Illumina Human Methylation 27")
+
+            query.met.samples <-  query.met$results[[1]]$cases
+
+            dataSmTP <- TCGAquery_SampleTypes(query.met.samples,"TP")
+            dataSmNT <- TCGAquery_SampleTypes(query.met.samples,"NT")
+
+            message <- paste0(CancerProject," ... with ", length(dataSmTP), " TP, ", length(dataSmNT), " NT")
+            print(message)
+
+
+            if(is.null(nSample)){
+                sampDown <- c(dataSmTP, dataSmNT)
+            }else{
+                sampDown <- c(sample(dataSmTP,nSample), sample(dataSmNT,nSample))
+            }
+
+            query.met <- GDCquery(project = CancerProject, 
+                                legacy = TRUE,
+                                data.category = "DNA methylation",
+                                platform = "Illumina Human Methylation 27",
+                                barcode = sampDown)
+
+            GDCdownload(query.met)
+
+            cancer.met <- GDCprepare(query = query.met,
+                                 save = TRUE, 
+                                 save.filename = FileNameData,
+                                 summarizedExperiment = TRUE)
+                                 
+            return(cancer.met)
       
-      query.met <- GDCquery(project = CancerProject, 
-                            legacy = TRUE,
-                            data.category = "DNA methylation",
-                            platform = "Illumina Human Methylation 27",
-                            barcode = sampDown)
-      
-      GDCdownload(query.met)
-      
-      cancer.met <- GDCprepare(query = query.met,
-                               save = TRUE, 
-                               save.filename = FileNameData,
-                               summarizedExperiment = TRUE)
-      
-      return(cancer.met)
-      
-    } else {
-      RSEobject <- get(load(FileNameData))
+        }else{
+            load(FileNameData)
+            RSEobject <- data
+        }
     }
-  }
 }
--- R/plotFEA.R
+++ R/plotFEA.R
@@ -10,95 +10,66 @@
 #' @param angle angle 
 #' @param xleg xleg 
 #' @param yleg yleg 
-#' @param bar.colors Colors of the bars.
 #' @param minY minY 
 #' @param maxY maxY 
-#' @importFrom reshape2 melt
-#' @import ggplot2
+#' @importFrom graphics barplot
+#' @importFrom graphics legend
+#' @importFrom graphics par
+#' @importFrom graphics text
+#' @importFrom grDevices pdf
+#' @importFrom grDevices dev.off
 #' @export
 #' @return no return value, FEA result is plotted
 #' @examples
 #' dataFEA <- FEA(DEGsmatrix = DEGsmatrix)
 #' plotFEA(dataFEA = dataFEA, additionalFilename = "_example",height = 20,width = 10)
-plotFEA <- function(dataFEA, 
-                    topBP = 10, 
-                    additionalFilename = NULL, 
-                    height, 
-                    width,
-                    bar.colors = c("darkgreen", "gold", "navy"),
-                    offsetValue = 5, 
-                    angle = 90, 
-                    xleg = 35, 
-                    yleg =5 , 
-                    minY = -5, 
-                    maxY =10){
+plotFEA <- function(dataFEA, topBP = 10, additionalFilename = NULL, height, width,
+                               offsetValue = 5, angle = 90, xleg = 35, yleg =5 , minY = -5, maxY =10){
+                               #offsetValue,angle,xleg=22,yleg=10,minY=-3,maxY=10){
   
-  par(mar = c(12,5,5,1))
-  dataFEA <- dataFEA[order(abs(dataFEA$Activation.z.score),decreasing =TRUE),]
-  
-  tmp <- dataFEA[1:topBP,]
-  
-  # tmp <- chosenFile[toupper(chosenFile[,"Filter"]) == "YES",]
-  
-  tmp <- as.data.frame(tmp)
-  tmp$p.Value <- as.numeric(tmp$p.Value)
-  tmp$Activation.z.score <- as.numeric(tmp$Activation.z.score)
-  tmp$commonNg <- as.numeric(tmp$commonNg)
-  
-  
-  tmp$p.Value <- -log2(tmp$p.Value)/10
-  
-  
-  toPlot <- matrix(0, nrow = 3, ncol = nrow(tmp))
-  #toPlot <- as.data.frame(toPlot) 
-  toPlot[1, ] <- tmp$p.Value
-  toPlot <- toPlot[, order(toPlot[1, ], decreasing = TRUE)]
-  toPlot[2, as.numeric(tmp$Activation.z.score) > 0] <- as.numeric(tmp$Activation.z.score[as.numeric(tmp$Activation.z.score) > 0])
-  toPlot[3, as.numeric(tmp$Activation.z.score) < 0] <- as.numeric(tmp$Activation.z.score[as.numeric(tmp$Activation.z.score) < 0])
-  
-  toPlot[1, which (toPlot[1, ] > 10)] <- 10 # check on y limits
-  
-  #toPlot <-as.numeric(toPlot)
-  simpleCap <- function(x) {
-    s <- x
-    paste(toupper(substring(s, first = 1, last = 1)), substring(s, 2),
-          sep="", collapse=" ")
-  }
-  colnames(toPlot) <-  paste(sapply(tmp$Diseases.or.Functions.Annotation, simpleCap), " (n=", tmp$commonNg, ")")
-  rownames(toPlot) <- c("-logFDR/10","Activation Z-score Increased","Activation Z-score Decreased")
-  
-  toPlot <- melt(toPlot)
-  colnames(toPlot) <- c("score","Biofunction","value")
-  p <- ggplot(toPlot, aes(x=Biofunction, y=value, fill=factor(score))) +
-    geom_bar(stat="identity", position="dodge", colour="white") +
-    theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-    ggtitle("FEA - Enriched BioFunctions") + theme(plot.title = element_text(lineheight=.8, face="bold")) +  xlab("") +  ylab("-logFDR/10 and Activation z-score") + labs(fill="") +
-    scale_fill_manual(values=bar.colors)  +  facet_grid(.~Biofunction,scales="free_x") + 
-    theme(panel.background = element_blank(),
-          panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank(),
-          axis.title.y=element_text(margin=margin(0,20,0,10)),
-          axis.text.x = element_text(size = 12),
-          strip.background = element_blank(),
-          strip.text.x = element_blank(),
-          axis.ticks.length = unit(.2, "cm"),
-          legend.key = element_rect(colour = 'white'),
-          #axis.text.y = element_text(angle = 90, hjust = 1),
-          legend.position="top",
-          axis.ticks.y = element_line(colour = "black"),
-          axis.line.y = element_line(colour = "black"),
-          panel.margin.x=unit(1.5, "lines"),
-          strip.text.y = element_blank()
-    ) +  scale_y_continuous(breaks=c(seq(minY,maxY,1)),expand = c(0, 0.6)) +
-    coord_cartesian(ylim = c(min(toPlot$value),max(toPlot$value))) +
-    geom_hline(aes(yintercept = 0),
-               colour = "black")
-  
+ # mycols <- c("#66C2A5", "#FC8D62", "#8DA0CB")
+    mycols <- c("#8DD3C7", "#FFFFB3", "#BEBADA")
 
-  if(!is.null(additionalFilename)){
-    ggsave(plot = p, file = paste0("plotFEA",additionalFilename,".pdf"))
-  } else {
-    plot(p)
-  }
-  
+    if(!is.null(additionalFilename)){
+        pdf(file = paste0("plotFEA",additionalFilename,".pdf"))
+    }
+    par(mar = c(12,5,5,1))
+    dataFEA <- dataFEA[order(abs(dataFEA$Activation.z.score),decreasing =TRUE),]
+
+    tmp <- dataFEA[1:topBP,]
+
+    # tmp <- chosenFile[toupper(chosenFile[,"Filter"]) == "YES",]
+
+    tmp <- as.data.frame(tmp)
+    tmp$p.Value <- as.numeric(tmp$p.Value)
+    tmp$Activation.z.score <- as.numeric(tmp$Activation.z.score)
+    tmp$commonNg <- as.numeric(tmp$commonNg)
+
+
+    tmp$p.Value <- -log2(tmp$p.Value)/10
+
+
+    toPlot <- matrix(0, nrow = 3, ncol = nrow(tmp))
+    #toPlot <- as.data.frame(toPlot) 
+    toPlot[1, ] <- tmp$p.Value
+    toPlot <- toPlot[, order(toPlot[1, ], decreasing = TRUE)]
+    toPlot[2, as.numeric(tmp$Activation.z.score) > 0] <- as.numeric(tmp$Activation.z.score[as.numeric(tmp$Activation.z.score) > 0])
+    toPlot[3, as.numeric(tmp$Activation.z.score) < 0] <- as.numeric(tmp$Activation.z.score[as.numeric(tmp$Activation.z.score) < 0])
+
+
+    toPlot[1, which (toPlot[1, ] > 10)] <-10 # check on y limits
+
+    #toPlot <-as.numeric(toPlot)
+    toPlot <- as.matrix(toPlot)
+    xAxis <- barplot(toPlot, beside = TRUE, col = mycols, ylab = "-logFDR/10 and Activation z-score",
+                   names = NULL, main = paste("FEA - Enriched BioFunctions",sep=" "), ylim = c(minY, maxY))
+    legend("topright", bty = "n", xleg, yleg, legend = c("-logFDR/10","Activation Z-score Increased","Activation Z-score Decreased"), 
+         fill = mycols, cex=0.5)#, text.col = c("blue","red", "green"), pch = 15)
+
+    text(xAxis[2, ], par("usr")[3]+offsetValue,  srt = angle, adj = 1, xpd = TRUE,
+       labels = paste(tmp$Diseases.or.Functions.Annotation, " (n=", tmp$commonNg, ")", sep = ""), cex = 1)
+
+    if(!is.null(additionalFilename)){
+        dev.off()
+    }
 }
