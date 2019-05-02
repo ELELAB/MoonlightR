453e007079e2b05fcc0f013981d281c033f20f34
Antonio Colaprico <antonio.colaprico@gmail.com>
2019-05-02 09:33:02 -0400

    Removing LPA from vignette
    Removing LPA from vignette

--- R/LPA.R
+++ R/LPA.R
@@ -4,7 +4,7 @@
 #' @param dataDEGs is output from DEA
 #' @param BP is biological process
 #' @param BPlist is list of genes annotated in BP
-#' @import RISmed 
+#' @import RISmed
 #' @importFrom utils txtProgressBar
 #' @importFrom utils setTxtProgressBar
 #' @export
@@ -13,11 +13,8 @@
 #' data(DEGsmatrix)
 #' BPselected <- c("apoptosis")
 #' BPannotations <- DiseaseList[[match(BPselected, names(DiseaseList))]]$ID
-#' dataLPA <- LPA(dataDEGs = DEGsmatrix[1:5,],
-#'                  BP =  BPselected,
-#'                  BPlist = BPannotations)
 LPA <- function (dataDEGs, BP, BPlist) {
-    
+
   BPgenesDEGs <- intersect(BPlist, rownames(dataDEGs))
   dataDEGsBP <- dataDEGs[BPgenesDEGs,]
 
@@ -31,46 +28,46 @@ LPA <- function (dataDEGs, BP, BPlist) {
                                "PubmedIncreases")
   DiseaseMN <- as.data.frame(DiseaseMN)
   DiseaseMN$Prediction..based.on.expression.direction. <- rep("Decreased", nrow(dataDEGsBP))
-  
+
   DiseaseMN$ID <- BPgenesDEGs
   DiseaseMN$Genes.in.dataset <- BPgenesDEGs
   DiseaseMN$Exp.Log.Ratio <- dataDEGs[BPgenesDEGs,"logFC"]
   rownames(DiseaseMN) <- DiseaseMN$ID
 
   pb <- txtProgressBar(min = 0, max = nrow(DiseaseMN), style = 3)
-  
+
   for ( i in 1: nrow(DiseaseMN)){
-  
+
     setTxtProgressBar(pb, i)
     curG <- DiseaseMN$ID[i]
-    
+
   keypubmed <- paste(curG,BP,"decreases")
   res <- EUtilsSummary(keypubmed, type="esearch", db="pubmed")
   fetch <- EUtilsGet(res,type="efetch", db="pubmed")
   RecordsDec <- length(fetch@PMID)
   DiseaseMN[curG,"PubmedDecreases"] <- RecordsDec
-  
+
   keypubmed <- paste(curG,BP,"increases")
   res <- EUtilsSummary(keypubmed, type="esearch", db="pubmed")
   fetch <- EUtilsGet(res,type="efetch", db="pubmed")
   RecordsInc <- length(fetch@PMID)
-  
+
   DiseaseMN[curG,"PubmedIncreases"] <- RecordsInc
-   
+
   if(DiseaseMN[curG,"PubmedDecreases"] == DiseaseMN[curG,"PubmedIncreases"]){
     DiseaseMN[curG,"Findings"] <- paste0("Affects (", RecordsInc, ")")
   }
-  
+
   if(DiseaseMN[curG,"PubmedDecreases"] < DiseaseMN[curG,"PubmedIncreases"]){
     DiseaseMN[curG,"Findings"] <- paste0("Increases (", RecordsInc, ")")
   }
-  
+
   if(DiseaseMN[curG,"PubmedDecreases"] > DiseaseMN[curG,"PubmedIncreases"]){
     DiseaseMN[curG,"Findings"] <- paste0("Decreases (", RecordsDec, ")")
   }
-  
+
   }
   close(pb)
-  
+
   return(DiseaseMN)
 }
